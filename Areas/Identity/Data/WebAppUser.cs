141958374694cedac4a7993be2ce6e531d9d3d6b
ChilliCate <286celeste@gmail.com>
2021-10-26 14:57:13 +0200

    Add Access Control (Log in & Register)

---
+++ Areas/Identity/Data/WebAppUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DigiMarketWebApp.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebAppUser class
+    public class WebAppUser : IdentityUser
+    {
+        [PersonalData]
+        [Column("First Name", TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column("Last Name", TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DigiMarketWebApp.Areas.Identity.Data;
+using DigiMarketWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DigiMarketWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace DigiMarketWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DigiMarketDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DigiMarketDbContextConnection")));
+
+                services.AddDefaultIdentity<WebAppUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireLowercase = false;
+                })
+                    .AddEntityFrameworkStores<DigiMarketDbContext>();
+            });
+        }
+    }
+}
