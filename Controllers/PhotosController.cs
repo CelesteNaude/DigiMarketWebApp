0f3766b90f2dc2979527a7557a0a7b4dafafa75a
ChilliCate <286celeste@gmail.com>
2021-11-21 19:54:13 +0200

    Fix Download Photo function

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -198,29 +198,6 @@ namespace DigiMarketWebApp.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        protected void btnDownload_OnClick(object sender, EventArgs e)
-        {
-            string filename = "~/Downloads/msizap.exe";
-            if (filename != "")
-            {
-                string path = Server.MapPath(filename);
-                System.IO.FileInfo file = new System.IO.FileInfo(path);
-                if (file.Exists)
-                {
-                    Response.Clear();
-                    Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
-                    Response.AddHeader("Content-Length", file.Length.ToString());
-                    Response.ContentType = "application/octet-stream";
-                    Response.WriteFile(file.FullName);
-                    Response.End();
-                }
-                else
-                {
-                    Response.Write("This file does not exist.");
-                }
-            }
-        }
-
         private bool PhotoExists(int id)
         {
             return _context.Photos.Any(e => e.PhotoID == id);
